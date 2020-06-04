8adcedcb6f43b6f3a06b56f5062f72f62c0cf068
sharfe25 <shadi2523@gmail.com>
2020-06-03 22:41:22 -0500

    Toolbar
    Se agrego toolbar a toda la página

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,36 @@
+<template>
+
+        <v-app-bar
+                color="amber darken-1"
+                dense
+                dark
+        >
+
+            <v-toolbar-title>El caminante</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            <v-btn text x-large>Home</v-btn>
+            <v-btn text x-large>Productos</v-btn>
+            <v-btn text x-large>Menú</v-btn>
+            <v-btn icon>
+                <v-icon>email</v-icon>
+            </v-btn>
+
+        </v-app-bar>
+
+</template>
+
+<script>
+    export default {
+        name: "Toolbar",
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
